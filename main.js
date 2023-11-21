fc125855e76cc9dd751f1ba7875b3fdbeb15d99b
jiechen <1487503910@qq.com>
2023-11-21 16:44:22 +0800

    fix: file path index problem

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const { bundlerOptions } = require("./config/env.js");
 
 const getBundlerOption = () => {
@@ -8,14 +9,20 @@ const getBundlerOption = () => {
 		BUNDLER = args[0].split("=")[1];
 	}
 
+	console.log('current bundler: ', BUNDLER);
+
 	return bundlerOptions[BUNDLER];
 };
 
 const main = () => {
 	try {
+		const content = fs.readFileSync('./examples/index.js', "utf-8")
+
 		const { entry, outputPath, loadBundler } = getBundlerOption();
-		console.log(outputPath);
 		loadBundler()(entry, outputPath);
+		// const absoluteEntry = path.resolve(__dirname, entry);
+		// const absoluteOutput = path.resolve(__dirname, outputPath);
+		// loadBundler()(absoluteEntry, absoluteOutput);
 	} catch (e) {
 		console.error(e);
 		process.exit(1);
