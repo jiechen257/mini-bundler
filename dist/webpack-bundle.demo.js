07f41fb82ce1045c9329c3289daa3408a100db5d
jiechen <1487503910@qq.com>
2024-04-02 12:51:05 +0800

    feat: update content

--- dist/webpack-bundle.demo.js
+++ dist/webpack-bundle.demo.js
@@ -1,41 +1,55 @@
-(function (graph) {
-  function require(module) {
-    function localRequire(relativePath) {
-      return require(graph[module].dependencies[relativePath]);
+
+// 统一扔到块级作用域中，避免污染全局变量
+// 为了方便，这里使用 {}，而不用 IIFE
+//
+// 以下代码为打包的三个重要步骤：
+// 1. 构建 modules
+// 2. 构建 webpackRequire，加载模块，模拟 CommonJS 中的 require
+// 3. 运行入口函数
+{
+  // 1. 构建 modules
+  const modules = [
+    
+  (function(exports, require, module) {
+    const inc = require(1).increment;
+const hello = require(3);
+const a = 1;
+console.log(inc(a));
+console.log(hello);
+  })
+,
+  (function(exports, require, module) {
+    const add = require(2).add;
+exports.increment = x => add(x, 1);
+  })
+,
+  (function(exports, require, module) {
+    exports.add = (...args) => args.reduce((x, y) => x + y, 0);
+  })
+,
+  (function(exports, require, module) {
+    module.exports = 'hello, world';
+  })
+
+  ]
+
+  // 模块缓存，所有模块都仅仅会加载并执行一次
+  const cacheModules = {}
+
+  // 2. 加载模块，模拟代码中的 require 函数
+  // 打包后，实际上根据模块的 ID 加载，并对 module.exports 进行缓存
+  function webpackRequire (moduleId) {
+    const cachedModule = cacheModules[moduleId]
+    if (cachedModule) {
+      return cachedModule.exports
     }
-
-    var exports = {};
-
-    (function (require, exports, code) {
-      eval(code);
-    })(localRequire, exports, graph[module].code);
-
-    return exports;
+    const targetModule = { exports: {} }
+    modules[moduleId](targetModule.exports, webpackRequire, targetModule)
+    cacheModules[moduleId] = targetModule
+    return targetModule.exports
   }
 
-  require('./src/index.js');
-})({
-  './src/index.js': {
-    dependencies: { './child.js': './src/child.js' },
-    code: 'use strict;var _child = require(./child.js);var family = this is our family, .concat(_child.son, , ).concat(_child.daughter);console.log(family);',
-  },
-  './src/child.js': {
-    dependencies: {
-      './mother.js': './src/mother.js',
-      './father.js': './src/father.js',
-    },
-    code: 'use strict;Object.defineProperty(exports, __esModule, {  value: true});exports.son = exports.daughter = void 0;var _mother = _interopRequireDefault(require(./mother.js));var _father = _interopRequireDefault(require(./father.js));function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var daughter = exports.daughter = this is daughter-Rachel, and .concat(_father[default]);var son = exports.son = this is son-Joy, and .concat(_father[default]);',
-  },
-  './src/mother.js': {
-    dependencies: { './grandpa.js': './src/grandpa.js' },
-    code: 'use strict;Object.defineProperty(exports, __esModule, {  value: true});exports[default] = void 0;var _grandpa = require(./grandpa.js);var mother = this is mother-Alice with .concat(_grandpa.grandpa);var _default = exports[default] = mother;',
-  },
-  './src/father.js': {
-    dependencies: { './grandpa.js': './src/grandpa.js' },
-    code: 'use strict;Object.defineProperty(exports, __esModule, {  value: true});exports[default] = void 0;var _grandpa = require(./grandpa.js);var father = this is father-Tom with .concat(_grandpa.grandpa);var _default = exports[default] = father;',
-  },
-  './src/grandpa.js': {
-    dependencies: {},
-    code: 'use strict;Object.defineProperty(exports, __esModule, {  value: true});exports.grandpa = void 0;var grandpa = exports.grandpa = grandpa-Jimmy;',
-  },
-});
+  // 3. 运行入口函数
+  webpackRequire(0)
+}
+
--- main.js
+++ main.js
@@ -1,3 +1,31 @@
-const bundle = require('./bundlers/webpack')
+const fs = require("fs");
+const { bundlerOptions } = require("./config/env.js");
 
-console.log(bundle('../../examples/index.js'))
+const getBundlerOption = () => {
+	const args = process.argv.slice(2);
+	let BUNDLER = "webpack";
+
+	if (args.length) {
+		BUNDLER = args[0].split("=")[1];
+	}
+
+	// console.log("current bundler: ", BUNDLER);
+
+	return bundlerOptions[BUNDLER];
+};
+
+// const bundle = require("./bundlers/webpack");
+
+
+const main = () => {
+	try {
+		const { entry, outputPath, loadBundler } = getBundlerOption();
+		// loadBundler()(entry, outputPath);
+		console.log(loadBundler()("../../examples/index.js"));
+	} catch (e) {
+		console.error(e);
+		process.exit(1);
+	}
+};
+
+main();
